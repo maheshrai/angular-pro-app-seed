9c6af4574315328f7173c5f5923b12747b208936
Mahesh Rai <mahesh.rai@gmail.com>
2017-08-26 05:26:44 -0700

    feat: added firebase authentication
    feat: added firebase authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
+export const ROUTES: Routes = [
+    {
+        path: 'auth',
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'login' },
+            { path: 'login', loadChildren: './login/login.module#LoginModule' },
+            { path: 'register', loadChildren: './register/register.module#RegisterModule' },
+        ]
+    }
+];
+
+export const firebaseConfig: FirebaseAppConfig = {
+    apiKey: "",
+    authDomain: "fitness-app-98aef.firebaseapp.com",
+    databaseURL: "https://fitness-app-98aef.firebaseio.com",
+    projectId: "fitness-app-98aef",
+    storageBucket: "fitness-app-98aef.appspot.com",
+    messagingSenderId: ""
+};
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(ROUTES),
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        SharedModule.forRoot()
+    ]
+})
+export class AuthModule { }
